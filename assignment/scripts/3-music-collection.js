72a1d181dd29eafa74f17166522e7d2a9cd87926
John Gaffaney <john.gaffaney@icloud.com>
2021-09-16 14:43:06 -0500

    created addToCollection, still need to work on the name of the object itself

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,19 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+//a function to accept the artist, title, and yearpublished
+//then create and object with those properties and add it to the Collection
+
+function addToCollection (title, artist, yearPublished) {
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }//end object
+  collection.push(album);
+  console.log(`${title} was added to collection.`);
+  return title;
+
+}//end addToCollection
+addToCollection('Eternol Idol', 'Black Sabbath', '1987');
